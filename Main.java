7047e72553cfc87ef94e747dbe6e171b8faa59b1
Jkhall81 <jason.kei.hall@gmail.com>
2023-12-06 15:16:50 -0700

    first commit.  Not much to say.  super basic.  learning Java.

---
+++ Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String[] guests = new String[10];
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.println();
+            System.out.println("1 - Display All Guests");
+            System.out.println("2 - Add Guest");
+            System.out.println("3 - Remove Guest");
+            System.out.println("4 - Exit");
+            System.out.print("Option: ");
+            int option = scanner.nextInt();
+            System.out.println();
+
+            if (option == 1) {
+                for (int i = 0; i < guests.length; i++) {
+                    System.out.println(guests[i]);
+                }
+            }
+            else if (option == 4) {
+                break;
+            }
+        } while (true);
+        System.out.println("Exiting...");
+    }
+}
